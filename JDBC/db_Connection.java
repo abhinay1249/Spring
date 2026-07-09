88e03be140604b7def4a41c0faa9470b8ea3564f
abhinay1249 <abhinaymarise@gmail.com>
2026-07-09 11:30:15 +0530

    Spring - JDBC

--- JDBC/db_Connection.java
+++ JDBC/db_Connection.java
@@ -4,14 +4,33 @@ import java.sql.DriverManager;
 
 public class db_Connection {
     
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        // Load or Register the Driver
+
         Class.forName("net.snowflake.client.api.driver.SnowflakeDriver");
         
-        String url = "https://dagljqb-ns91258.snowflakecomputing.com";
+        // Establish a Connection
+
+        String url = "jdbc:snowflake://dagljqb-ns91258.snowflakecomputing.com?db=PRODUCTION&schema=Hyderbad_Team&warehouse=COMPUTE_WH&role=ACCOUNTADMIN";
         String user = "abhinay";
         String password = "Kasmo@123456789";
 
         Connection conn = DriverManager.getConnection(url,user,password);
+
+        // Create A Statement
+
+        Statement statement = conn.createStatement();
+
+
+        // Execute Query
+        // Process the result
+
+
+        // Close the Connection
+        
+        statement.close();
+        conn.close();
 
 
     }
